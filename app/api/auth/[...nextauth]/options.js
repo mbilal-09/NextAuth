4477b025338074ac0d432bb62c5084ab960bdff1
mbilal-09 <bilalkhanhsakir@gmail.com>
2024-03-26 16:02:57 +0500

    providers added

--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -1,4 +1,40 @@
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
-export const options = {}
+export const options = {
+    providers: [
+        GithubProvider({
+            profile(profile) {
+                console.log("Github Profile: ", profile);
+
+                let userRole = "Github User";
+                if (profile?.email == "bilalkhanshakir@gmail.com") {
+                    userRole = "admin";
+                }
+
+                return {
+                    ...profile,
+                    role: userRole,
+                };
+            },
+            clientId: process.env.GITHUB_ID,    
+            clientSecret: process.env.GITHUB_SECRET,
+        }),
+        GoogleProvider({
+            profile(profile) {
+                console.log("Google Profile: ", profile);
+
+                return {
+                    ...profile,
+                    id: profile.sub,
+                    role: userRole,
+                };
+            },
+            clientId: process.env.GOOGLE_ID,    
+            clientSecret: process.env.GOOGLE_SECRET,
+        }),
+    ],
+    callbacks: {
+        // async jwt({})
+    }
+};
