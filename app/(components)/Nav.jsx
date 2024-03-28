696e8023bc6b78950ee70019a6a1119416139e26
mbilal-09 <bilalkhanshakir@gmail.com>
2024-03-28 08:58:41 +0500

    authentication/protected routes

--- app/(components)/Nav.jsx
+++ app/(components)/Nav.jsx
@@ -1,21 +1,30 @@
+import { getServerSession } from 'next-auth'
 import Link from 'next/link'
 import React from 'react'
+import { options } from '../api/auth/[...nextauth]/options'
 
-const Nav = () => {
+const Nav = async () => {
+  const session = await getServerSession(options);
+
   return (
-    <header className='bg-gray-500 text-gray-100'>
-        <nav className='flex justify-between items-center w-full px-10 py-4'>
-            <div>Mysite</div>
-            <div className='flex gap-8'>
-                <Link href={"/"}>Home</Link>
-                <Link href={"/CreateUser"}>Create User</Link>
-                <Link href={"/ClientMember"}>Client Member</Link>
-                <Link href={"/Member"}>Member</Link>
-                <Link href={"/Public"}>Public</Link>
-            </div>
-        </nav>
+    <header className="bg-gray-500 text-gray-100">
+      <nav className="flex justify-between items-center w-full px-10 py-4">
+        <div>Mysite</div>
+        <div className="flex gap-8">
+          <Link href={"/"}>Home</Link>
+          <Link href={"/CreateUser"}>Create User</Link>
+          <Link href={"/ClientMember"}>Client Member</Link>
+          <Link href={"/Member"}>Member</Link>
+          <Link href={"/Public"}>Public</Link>
+          {session ? (
+            <Link href={"/api/auth/signout?callbackUrl=/"}>Log Out</Link>
+          ) : (
+            <Link href={"/api/auth/signin"}>LogIn</Link>
+          )}
+        </div>
+      </nav>
     </header>
-  )
+  );
 }
 
 export default Nav
--- app/ClientMember/page.jsx
+++ app/ClientMember/page.jsx
@@ -1,9 +1,23 @@
 "use client";
 
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
 const Member = () => {
+  const router = useRouter();
+  const {data: session} = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.push("/api/auth/signin?callbackUrl=/ClientMember");
+    },
+  });
+  
   return (
     <div>
       <h1>Member Client Session</h1>
+      <p>{session?.user?.email}</p>
+      <p>{session?.user?.name}</p>
+      <p>{session?.user?.role}</p>
     </div>
   );
 };
